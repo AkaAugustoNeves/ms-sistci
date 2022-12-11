be76272b95c953be5f5daa6e62973d26626601bf
AkaAugustoNeves <augusto.nevesti@gmail.com>
2022-12-10 21:05:59 -0300

    criação do ms sistci

---
+++ ms-sistci/src/main/java/br/com/pilares/mssistci/MsSistciApplication.java
@@ -0,0 +1,13 @@
+package br.com.pilares.mssistci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSistciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSistciApplication.class, args);
+	}
+
+}
---
+++ ms-sistci/src/test/java/br/com/pilares/mssistci/MsSistciApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pilares.mssistci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSistciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
